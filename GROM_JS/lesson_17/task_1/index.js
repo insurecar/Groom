5acbf08b45b73c83f06d50f37577bd45023229c1
insurecar <81600@ukr.net>
2020-04-30 17:40:49 +0300

    qw

--- GROM_JS/lesson_17/task_1/index.js
+++ GROM_JS/lesson_17/task_1/index.js
@@ -1,48 +1,50 @@
 /* ===> 1 <=== */
 const student = {
-    name: 'Tom',
+  name: "Tom",
 };
 
 export function sayName() {
-    console.log(this.name);
+  console.log(this.name);
 }
 
-// вызовите ф-цию sayName так, чтобы в консоль вывелось имя студента 
+// вызовите ф-цию sayName так, чтобы в консоль вывелось имя студента
 // ... your code here
 sayName.call(student);
 // вызовите ф-цию sayName так, чтобы в консоль вывелось имя 'Bruce' (используйте другой объект)
 // ... your code here
 const student2 = {
-    name: 'Bruce',
+  name: "Bruce",
 };
 sayName.call(student2);
 
 /* ===> 2 <=== */
 const company = {
-    companyName: 'Microsoft'
+  companyName: "Microsoft",
 };
 
 function greeting(firstName, lastName) {
-    console.log(`Hello, ${firstName} ${lastName}. Welcome to the ${this.companyName}`);
+  console.log(
+    `Hello, ${firstName} ${lastName}. Welcome to the ${this.companyName}`
+  );
 }
 
-// вызовите ф-цию greeting так, чтобы в консоль вывелось 
+// вызовите ф-цию greeting так, чтобы в консоль вывелось
 // 'Hello, Bob Marley. Welcome to the Microsoft'
 // используйте объект company
 // ... your code here
-greeting.call(company, 'Bob', 'Marley')
+greeting.call(company, "Bob", "Marley");
 
 /* ===> 3 <=== */
 const country = {
-    countryName: 'Ukraine',
-    capital: 'Kyiv'
+  countryName: "Ukraine",
+  capital: "Kyiv",
 };
 
 function getPopulation(population) {
-    return `Population in ${this.countryName} is ${population}`;
+  return `Population in ${this.countryName} is ${population}`;
 }
 
-// вызовите ф-цию getPopulation так, чтобы она вернула 
+// вызовите ф-цию getPopulation так, чтобы она вернула
 // 'Population in Ukraine is 43000'
 // используйте объект country
 // ... your code here
@@ -50,20 +52,22 @@ console.log(getPopulation.call(country, 43000));
 
 /* ===> 4 <=== */
 const transaction = {
-    amount: 1200,
-    operation: 'sell',
-    currency: 'USD',
-    exchange: 'NYSE',
-    printTransaction() {
-        console.log(`${this.amount} ${this.currency} - ${this.operation} on ${this.exchange}`);
-    }
-}
+  amount: 1200,
+  operation: "sell",
+  currency: "USD",
+  exchange: "NYSE",
+  printTransaction() {
+    console.log(
+      `${this.amount} ${this.currency} - ${this.operation} on ${this.exchange}`
+    );
+  },
+};
 
 const anotherTransaction = {
-    amount: 400,
-    operation: 'buy',
-    currency: 'USD',
-    exchange: 'NASDAQ',
+  amount: 400,
+  operation: "buy",
+  currency: "USD",
+  exchange: "NASDAQ",
 };
 
 // вызовите метод transaction.printTransaction так, чтобы в консоль вывелось
--- REACT_GROM/lesson_4/hw_1/src/Clock.jsx
+++ REACT_GROM/lesson_4/hw_1/src/Clock.jsx
@@ -1,40 +1,36 @@
-import React, { Component } from 'react'
-import './clock.scss';
+import React, { Component } from "react";
+import "./clock.scss";
 
 const getTime = (timezone) => {
   const currentTimeZone = new Date().getTimezoneOffset() / 60;
-  const locationTimeZone = new Date(new Date().setHours(new Date().getHours() + currentTimeZone + timezone));
+  const locationTimeZone = new Date(
+    new Date().setHours(new Date().getHours() + currentTimeZone + timezone)
+  );
   return locationTimeZone;
-}
+};
 
 class Clock extends Component {
   constructor(props) {
     super(props);
     this.state = {
       location: props.location,
-      time: getTime(props.offset).toLocaleTimeString('en-US'),
-    }
+      time: getTime(props.offset).toLocaleTimeString("en-US"),
+    };
 
     setInterval(() => {
       this.setState({
-        time: getTime(props.offset).toLocaleTimeString('en-US'),
-      })
-    }, 1000)
+        time: getTime(props.offset).toLocaleTimeString("en-US"),
+      });
+    }, 1000);
   }
-  render () {
+  render() {
     return (
       <div className="clock">
-        <div className="clock__location">
-        {this.state.location}
-        </div>
-        <div className="clock__time">
-        {this.state.time}
-        </div>
+        <div className="clock__location">{this.state.location}</div>
+        <div className="clock__time">{this.state.time}</div>
       </div>
-    )
+    );
   }
 }
 
 export default Clock;
-
-
