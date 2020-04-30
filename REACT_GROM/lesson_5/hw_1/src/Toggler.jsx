af5009a02001d42b2f8f0e7dd23886a788b7e02f
insurecar <81600@ukr.net>
2020-04-30 19:50:59 +0300

    qw

---
+++ REACT_GROM/lesson_5/hw_1/src/Toggler.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class Toggle extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isToggleOn: false,
+    };
+  }
+  handleClick = () => {
+    this.setState({
+      isToggleOn: !this.state.isToggleOn,
+    });
+  };
+  render() {
+    return (
+      <div onClick={this.handleClick} className="toggler">
+        {this.state.isToggleOn ? "On" : "Off"}
+      </div>
+    );
+  }
+}
+
+export default Toggle;
