55664f664027093ccc068b351e738a3c5588a502
insurecar <81600@ukr.net>
2020-04-30 19:25:16 +0300

    qw

--- REACT_GROM/lesson_5/task_4/src/index.jsx
+++ REACT_GROM/lesson_5/task_4/src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Colors from "./Color";
+import Colors from "./Colors";
 import "./index.scss";
 
 const rootElement = document.querySelector("#root");
