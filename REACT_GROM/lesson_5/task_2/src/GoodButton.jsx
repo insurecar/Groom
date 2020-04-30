72af2f46fc21c64b5ae628ed4f9ea7b357bb3990
insurecar <81600@ukr.net>
2020-04-30 18:05:03 +0300

    qw

---
+++ REACT_GROM/lesson_5/task_2/src/GoodButton.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class GoodButton extends Component {
+  handleClick(e) {
+    console.log(e);
+
+    alert("Good job!");
+  }
+  render() {
+    return (
+      <button className="fancy-button" onClick={this.handleClick}>
+        Click me!
+      </button>
+    );
+  }
+}
+
+export default GoodButton;
