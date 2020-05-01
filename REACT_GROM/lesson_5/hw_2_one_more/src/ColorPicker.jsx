0ba01ca21f783b008398661b8c6bedc35e2e7d5c
insurecar <81600@ukr.net>
2020-05-01 13:07:56 +0300

    qw

---
+++ REACT_GROM/lesson_5/hw_2_one_more/src/ColorPicker.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+
+class ColorPicker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+    };
+  }
+  setTitle = (colorName) => {
+    this.setState({
+      title: colorName,
+    });
+  };
+
+  reset = () => {
+    this.setState({
+      title: "",
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="picker__title">{this.state.title}</div>
+        <div>
+          <button
+            className="picker__button picker__button_coral"
+            onMouseOver={() => this.setTitle("Coral")}
+            onMouseOut={this.reset}
+          />
+          <button
+            className="picker__button picker__button_aqua"
+            onMouseOver={() => this.setTitle("Aqua")}
+            onMouseOut={this.reset}
+          />
+          <button
+            className="picker__button picker__button_bisque"
+            onMouseOver={() => this.setTitle("Bisque")}
+            onMouseOut={this.reset}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ColorPicker;
