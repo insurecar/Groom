1510dbbbde4291482c377eb495f872c05132b804
insurecar <81600@ukr.net>
2020-05-05 12:12:14 +0300

    qw

---
+++ REACT_GROM/lesson_9/task_3/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import UserForm from "./UserForm";
+
+class App extends Component {
+  createUser = (user) => console.log(user);
+
+  render() {
+    return <UserForm handleSubmit={this.createUser} />;
+  }
+}
+
+export default App;
