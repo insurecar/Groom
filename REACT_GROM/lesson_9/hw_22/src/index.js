473eb0fc95f2cd21efbb13bbc6beddeda6bebdea
insurecar <81600@ukr.net>
2020-05-07 14:46:21 +0300

    qw

---
+++ REACT_GROM/lesson_9/hw_22/src/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Game from "./Game";
+
+const rootElement = document.querySelector("#root");
+
+ReactDOM.render(<Game />, rootElement);
--- REACT_GROM/lesson_9/zero/src/Square.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class Square extends React.Component {
-  render() {
-    return <button className="square">{/* TODO */}</button>;
-  }
-}
-
-export default Square;
