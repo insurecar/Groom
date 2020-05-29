3cd71339e5c0ce4ef4015c787bfef001a6c66784
insurecar <81600@ukr.net>
2020-05-29 13:40:43 +0300

    qw

---
+++ REACT_GROM/lesson_8/one_more/task_1/src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Demo from "./Demo";
+import "./index.scss";
+const rootElement = document.querySelector("#root");
+
+ReactDOM.render(<Demo />, rootElement);
