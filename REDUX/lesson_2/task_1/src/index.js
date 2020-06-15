27451242f290c53323709ff26a3b894a44e3c328
insurecar <81600@ukr.net>
2020-06-15 10:35:57 +0300

    qw

---
+++ REDUX/lesson_2/task_1/src/index.js
@@ -0,0 +1,31 @@
+import { createStore } from "redux";
+
+const incrementAction = {
+  type: "INCREMENT",
+};
+
+const decrementAction = {
+  type: "DECREMENT",
+};
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+const store = createStore(counterReducer);
+store.dispatch(incrementAction);
+store.dispatch(incrementAction);
+store.dispatch(incrementAction);
+store.dispatch(incrementAction);
+store.dispatch(decrementAction);
+store.dispatch(decrementAction);
+store.dispatch(decrementAction);
+store.dispatch(decrementAction);
+
+console.log(store.getState());
