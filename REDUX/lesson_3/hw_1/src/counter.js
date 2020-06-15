82349b49f645fa232e7c040b19ab02c847ac2d58
insurecar <81600@ukr.net>
2020-06-15 17:06:04 +0300

    qw

--- REDUX/lesson_3/hw_1/src/counter.js
+++ REDUX/lesson_3/hw_1/src/counter.js
@@ -1,7 +1,7 @@
 import { createStore } from "redux";
 
-export const INCREMENT = "COUNTER/INCREMENT";
-export const DECREMENT = "COUNTER/DECREMENT";
+const INCREMENT = "COUNTER/INCREMENT";
+const DECREMENT = "COUNTER/DECREMENT";
 
 export const increment = () => {
   return {
@@ -15,14 +15,12 @@ export const decrement = () => {
   };
 };
 
-export const counterReducer = (state = 0, action) => {
+const counterReducer = (state = 0, action) => {
   switch (action.type) {
     case INCREMENT:
       return state + 1;
-
     case DECREMENT:
       return state - 1;
-
     default:
       return state;
   }
